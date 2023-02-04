c87cf4eebb568e57941a743a6e3874d914269f6c
Adam Halasa <adam@tworcastron.pl>
2023-02-04 15:03:34 +0100

    typeof

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const product1 = { price: 10 }
 const product2 = { price: '20' }
 const discount = 10
 
+console.log(typeof product1.price);
+console.log(typeof '20');
+console.log(typeof '20');
+
 const total = product1.price + product2.price
 const totalWithDiscount = total - 10
 
