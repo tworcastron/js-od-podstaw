c7a8006bf8b4cdf5b0415d23425f9d710cf9c438
Adam Halasa <adam@tworcastron.pl>
2023-02-07 18:18:08 +0100

    Konwersja typow

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const product1 = { price: 10 }
-const product2 = { price: '20' }
-const discount = 10
+const product1 = { price: 10 };
+const product2 = { price: '20' };
+const discount = 10;
 
-console.log(typeof product1.price);
-console.log(typeof '20');
-console.log(typeof '20');
+// String(), Number(), Boolean() !!
+if (isNaN(+product2.price)) {
+  console.log('Podano niepoprawny typ danych');
+}
 
-const total = product1.price + product2.price
-const totalWithDiscount = total - 10
+const total = Number(product1.price) + Number(product2.price);
+const totalWithDiscount = total - 10;
 
 console.log(`
   Cena przed zniżką: ${total}
   Cena po zniżce: ${totalWithDiscount}
-`)
+`);
