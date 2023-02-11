1629fab5d6a1340a72628fcdd8f5cd9d6bda20c1
Adam Halasa <adam@tworcastron.pl>
2023-02-11 13:19:47 +0100

    Usuwanie elementow DOM

--- src/js/app.js
+++ src/js/app.js
@@ -9,17 +9,38 @@ const discountCheckbox = document.querySelector('#add-discount')
 
 // dodaj produkty do tabeli
 const itemsContainer = document.querySelector('#items');
-let counter = 1;
 function addItem(item) {
   itemsContainer.innerHTML += `<tr>
-          <td>${counter++}</td>
+          <td><button class="delete">x</button></td>
           <td>${item.title}</td>
-          <td><input type="number" value="1"></td>
+          <td><input class="quantity" type="number" value="1"></td>
           <td>${item.price}</td>
         </tr>`;
 }
 addItem(product1);
 addItem(product2);
+
+// usuwanie wierszy
+const deleteButtons = document.querySelectorAll('.delete');
+for (let i = 0; i < deleteButtons.length; i++) {
+  deleteButtons[i].addEventListener('click', removeRow);
+}
+
+const quantityInputs = document.querySelectorAll('.quantity');
+for (let i = 0; i < quantityInputs.length; i++) {
+  quantityInputs[i].addEventListener('change', removeRowFromQuantity);
+}
+
+function removeRow(e) {
+  const row = e.target.closest('tr');
+  row.remove();
+}
+function removeRowFromQuantity(e) {
+  if (Number(e.target.value) === 0) {
+    const row = e.target.closest('tr');
+    row.remove();
+  }
+}
 
 // zmień kolo tła wiersza
 function markBg(e) {
