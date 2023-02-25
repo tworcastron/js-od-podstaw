ab2e90a932e14d402f8947a46c686b5733e2d246
Adam Halasa <adam@tworcastron.pl>
2023-02-25 16:35:33 +0100

    && oraz ||

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,30 @@
-const x = '';
+// && (AND) - i/oraz
+// || (OR) - lub
 
-// if (x)
-//   console.log('jest prawdziwa');
-// else
-//   console.log('jest prawdziwa');
+const DISCOUNT = true;
+const course = {
+  price: 12,
+  discount: {
+    exists: true,
+    amount: 2,
+  },
+};
+const course2 = {
+  price: 12,
+};
 
-// x
-//   ? console.log('jest prawdziwa')
-//   : console.log('jest prawdziwa')
-
-
-// let number;
-// if (x) {
-//   number = 2;
-// } else {
-//   number = 4;
+// if (course2.discount) {
+//   console.log(course2.discount.amount);
 // }
+// console.log(course.discount && course.discount.amount);
 
-// const number = x ? 2 : 4;
+// console.log(DISCOUNT && course);
+// console.log(DISCOUNT && false);
+// console.log(false && DISCOUNT);
+// console.log(DISCOUNT || course);
+// console.log(DISCOUNT || false);
+// console.log(false || course);
+
+// const input = window.prompt('Podaj imię');
+// const userName = input || 0;
+// console.log(userName);
