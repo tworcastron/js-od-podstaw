acbfb225f2eaaf024a71ac86455460decba51939
Adam Halasa <adam@tworcastron.pl>
2023-02-25 17:31:48 +0100

    Optional chaining ?.

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,9 @@
-// && (AND) - i/oraz
-// || (OR) - lub
-
-const DISCOUNT = true;
-const course = {
-  price: 12,
-  discount: {
-    exists: true,
-    amount: 2,
+// optional chaining ?.
+const animals = [
+  {
+    name: 'dog',
+    bark: function () { console.log('how how'); }
   },
-};
-const course2 = {
-  price: 12,
-};
-
-// if (course2.discount) {
-//   console.log(course2.discount.amount);
-// }
-// console.log(course.discount && course.discount.amount);
-
-// console.log(DISCOUNT && course);
-// console.log(DISCOUNT && false);
-// console.log(false && DISCOUNT);
-// console.log(DISCOUNT || course);
-// console.log(DISCOUNT || false);
-// console.log(false || course);
-
-// const input = window.prompt('Podaj imię');
-// const userName = input || 0;
-// console.log(userName);
+  { name: 'cat' },
+];
+animals[1]?.bark?.() 
