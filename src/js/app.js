6d063f28a789480b555af62d49f0fe290be615bb
Adam Halasa <adam@tworcastron.pl>
2023-02-25 13:03:07 +0100

    if, else

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
-// const addItem = function() {}
+const x = '';
+
+// if (x)
+//   console.log('jest prawdziwa');
+// else
+//   console.log('jest prawdziwa');
+
+// x
+//   ? console.log('jest prawdziwa')
+//   : console.log('jest prawdziwa')
+
+
+// let number;
+// if (x) {
+//   number = 2;
+// } else {
+//   number = 4;
+// }
+
+// const number = x ? 2 : 4;
