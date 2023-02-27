226b7a064f3ef701bab9239b87021ff972815449
Adam Halasa <adam@tworcastron.pl>
2023-02-27 08:21:02 +0100

    Switch

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,27 @@
-// Nullish coalescing operator: ??
+// switch
+const color = 'azure';
 
-const user = {
-  // points: 0,
+// if (color === 'green') {
+// 	console.log('zielony');
+// } else if (color === 'red') {
+// 	console.log('czerwony');
+// }else if (color === 'blue' || color === 'azure') {
+// 	console.log('niebieski');
+// } else {
+// 	console.log('brak koloru');
+// }
+
+switch (color) {
+  case 'green':
+    console.log('zielony');
+    break;
+  case 'red':
+    console.log('czerwony');
+    break;
+  case 'blue':
+  case 'azure':
+    console.log('niebieski');
+    break;
+  default:
+    console.log('brak koloru');
 }
-
-// [null, undefined, '', false, 0, -0, 0n, NaN]
-console.log(user.points || 'wczytywanie');
-
-// [null, undefined]
-console.log(user.points ?? 'wczytywanie');
