63f9e2d87d0610d018e5ea0adff746955cc3171b
Adam Halasa <adam@tworcastron.pl>
2023-03-04 17:37:38 +0100

    domkniecia

--- src/js/app.js
+++ src/js/app.js
@@ -2,25 +2,32 @@
 const coursesList = document.querySelector('.coursers-list');
 const counter = document.querySelector('.counter');
 
-// lista produktów
-const items = [];
+function createCart() {
+  // // lista produktów
+  const items = [];
 
-const refreshProductsCount = () => {
-  counter.innerText = items.length;
+  const refreshProductsCount = () => counter.innerText = items.length;
+
+  const add = (title, price, quantity = 1) => { // parametry
+    items.push({ title, price, quantity });
+    refreshProductsCount();
+    console.log(items);
+  }
+
+  return {
+    add,
+  };
 }
 
-const addToCart = (title, price, quantity = 1) => { // parametry
-  items.push({ title, price, quantity });
-  refreshProductsCount();
-  console.log(items);
-}
+const cart = createCart();
+console.log(cart);
 
 // obsługa buttona
 const addToCartHandler = (e) => {
   if (e.target.tagName !== 'BUTTON') return;
   const title = e.target.dataset.title;
   const price = Number(e.target.dataset.price);
-  addToCart(title, price); // argumenty
+  cart.add(title, price); // argumenty
 }
 
 // listenery
