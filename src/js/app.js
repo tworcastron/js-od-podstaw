0dccfdedcbefa749452afea1257fa456ca421fc8
Adam Halasa <adam@tworcastron.pl>
2023-03-04 17:01:02 +0100

    counter

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,17 @@
 // definicja elementow
 const coursesList = document.querySelector('.coursers-list');
+const counter = document.querySelector('.counter');
 
 // lista produktów
 const items = [];
 
+const refreshProductsCount = () => {
+  counter.innerText = items.length;
+}
+
 const addToCart = (title, price, quantity = 1) => { // parametry
   items.push({ title, price, quantity });
+  refreshProductsCount();
   console.log(items);
 }
 
