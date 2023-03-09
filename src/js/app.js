5166d5f8110f3afe63406afdcb1d41528b543b9f
Adam Halasa <adam@tworcastron.pl>
2023-03-09 13:34:53 +0100

    nazewnictwo zmiennych

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,5 @@
-// rekurencja
 
-// const getSum = (start, ...args) => {
-//   if (args.length === 0) return start;
+// Funkcja to CZASOWNIK / frazy czasownikowe (np. policz, dodaj, usuń)
 
-//   return args[0] + getSum(start, ...args.slice(1));
-// }
-
-// console.log(getSum(0, 2, 2, 5));
-
-let x = 0; // 10
-
-const addUntill = (a, untill) => {
-  if (a === untill) return a;
-
-  return addUntill(a + 1, untill);
-}
-
-console.log(addUntill(x, 10));
+const add = () => {}
+const item = {};
