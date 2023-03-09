e5c13710da0fce61d23b4eaae4326b263d6e1334
Adam Halasa <adam@tworcastron.pl>
2023-03-09 12:34:44 +0100

    spread operator w funkcji

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,9 @@
-// callback
+// spread operator ...
 
-// asynchroniczna
-// const prepareMeal = (callback) => {
-//   setTimeout(() => {
-//     console.log('zbieranie składniów');
-//     console.log('przygotowywanie');
-//     callback();
-//   }, 2000);
-// }
-// const notify = () => {
-//   console.log('Danie gotowe!');
-// }
-
-// prepareMeal(notify);
-
-
-const add = (a, b) => a + b;
-const sub = (a, b) => a - b;
-
-const doSmthWithNumber = (x, y, callback) => {
-  console.log(callback(x, y));
+const getSum = (start, ...args) => {
+  let sum = start;
+  args.forEach(a => sum += a);
+  console.log(args, sum);
 }
 
-doSmthWithNumber(2, 5, add);
-doSmthWithNumber(2, 5, sub);
-
-
-// const callback = (a) => {
-//   return this.price += item.price;
-// }
-// this.items.forEach(callback);
+getSum(-4, 2, 2);
