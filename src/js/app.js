c9a4366a8e71df32efb16c9cee9cae8cb0e80f08
Adam Halasa <adam@tworcastron.pl>
2023-03-09 13:16:55 +0100

    rekurencja

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,19 @@
-// spread operator ...
+// rekurencja
 
-const getSum = (start, ...args) => {
-  let sum = start;
-  args.forEach(a => sum += a);
-  console.log(args, sum);
+// const getSum = (start, ...args) => {
+//   if (args.length === 0) return start;
+
+//   return args[0] + getSum(start, ...args.slice(1));
+// }
+
+// console.log(getSum(0, 2, 2, 5));
+
+let x = 0; // 10
+
+const addUntill = (a, untill) => {
+  if (a === untill) return a;
+
+  return addUntill(a + 1, untill);
 }
 
-getSum(-4, 2, 2);
+console.log(addUntill(x, 10));
