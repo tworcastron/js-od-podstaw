775b25fda2860b7f595172582fe3fcf7e01558d6
Adam Halasa <adam@tworcastron.pl>
2023-04-01 18:22:24 +0200

    Formularz - walidacja JS

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,41 @@
+const validateName = (value) => {
+  if (!value) return 'Imię i nazwisko jest wymagane';
+  if (value.length < 3) return 'Imię i nazwisko jest za krótkie';
+}
+const validateTel = (value) => {
+  if (!value) return 'Telefon jest wymagany';
+  if (value.length < 9) return 'Telefon jest niepoprawny';
+}
+const validateEmail = (value) => {
+  if (!value) return 'Email jest wymagany';
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!regex.test(value)) return 'Email nie jest poprawny';
+}
+const validateEmailConfirm = (value, email) => {
+  if (value !== email) return 'Email jest inny';
+}
+
+const validate = (key, value, allValues) => {
+  switch (key) {
+    case 'name': return validateName(value);
+    case 'tel': return validateTel(value);
+    case 'email': return validateEmail(value);
+    case 'email-confirm': return validateEmailConfirm(value, allValues.email);
+  }
+}
+
+const validateValues = (values) => {
+  const errors = [];
+  Object.entries(values).forEach(([key, value]) => {
+    const error = validate(key, value, values);
+    if (error) errors.push(error);
+  })
+
+  document.querySelector('#errors').innerHTML = errors
+    .map(e => `<li>${e}</li>`)
+    .join('');
+}
+
 const onSubmit = (e) => {
   e.preventDefault();
 
@@ -9,7 +47,8 @@ const onSubmit = (e) => {
     tel: elements['tel'].value,
     payment: elements['payment'].value,
   };
-  console.log(values);
+
+  validateValues(values);
 }
 
 // podpięcie formularza
