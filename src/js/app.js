415a1ba710b848d1fbd91e192b1ae3eabe3a48b5
Adam Halasa <adam@tworcastron.pl>
2023-04-04 18:24:52 +0200

    callback hell

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,48 @@
+// sprawdzenie czy produkty są wciąż aktualne
+// sprawdzenie czy cena jest poprawna
+// złożenie zamówienia
+// pobranie promocji (w podziękowaniu) do zamówienia
 
-console.log('jeden');
+const checkProducts = (data, callback) => {
+  // wysłanie requestu na backend
+  setTimeout(() => {
+    callback({ status: 'ok' });
+  }, 2000);
+}
+const checkPrice = (data, callback) => {
+  setTimeout(() => {
+    callback({ status: 'ok', data })
+  }, 2000);
+}
+const makeOrder = (data, callback) => {
+  setTimeout(() => {
+    callback({ orderId: 123, data })
+  }, 2000);
+}
+const checkPromotionForOrder = (orderId, callback) => {
+  setTimeout(() => {
+    callback(['kurs HTML za 50%!'])
+  }, 2000);
+}
 
-setTimeout(() => {
-  console.log('dwa')
-}, 0);
+// callback hell (kod wygląda jak choinka...)
+const orderData = {}; // dane zamówienia
+checkProducts(orderData, (response) => {
+  const { status } = response;
+  console.log('Czy produkty dostępne: ', status);
 
-console.log('trzy');
+  checkPrice(orderData, (response2) => {
+    const { status, data } = response2;
+    console.log('Cena poprawna: ', status);
+
+    makeOrder(orderData, (response3) => {
+      const { orderId, data } = response3;
+      console.log('Order ID', orderId);
+
+      checkPromotionForOrder(orderId, (response4) => {
+        const promo = response4;
+        console.log('Promocje: ', promo);
+      });
+    });
+  });
+});
