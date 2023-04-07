3ce7f2d96903d512605a84097d0e4a2bd5d09100
Adam Halasa <adam@tworcastron.pl>
2023-04-07 10:32:11 +0200

    fetch - obluga bledu

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,15 @@
-// DELETE
+// DELETE (błąd)
 
 fetch('https://jsonplaceholder.typicode.com/pos/4', {
   method: 'DELETE',
 })
-  .then(res => res.json())
+  .then(res => {
+    if (!res.ok) {
+      throw new Error('Dane nie mogły zostać usunięte!');
+    }
+    return res.json()
+  })
   .then(res => console.log(res))
+  .catch(error => {
+    console.log('Błąd: ', error.message);
+  });
