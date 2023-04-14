f539708f42eecebf8518fde5431cc5270f4da01f
Adam Halasa <adam@tworcastron.pl>
2023-04-14 12:22:01 +0200

    zewnetrzne biblioteki

--- src/js/cookies.js
+++ src/js/cookies.js
@@ -1,6 +1,8 @@
 // import { setCookie, getCookie, COOKIE_NAME } from './helpers/cookie.js';
 import * as cookie from './helpers/cookie.js';
 
+// console.log(Cookies.get('text'));
+
 const d = new Date();
 d.setHours(d.getHours() + 1);
 const utc = d.toUTCString();
@@ -8,4 +10,4 @@ const utc = d.toUTCString();
 cookie.setCookie('text', encodeURIComponent(`tytuł
 opis`), 1);
 // deleteCookie('text');
-console.log(cookie.getCookie('text'));
+// console.log(cookie.getCookie('text'));
