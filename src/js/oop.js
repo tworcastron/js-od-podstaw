d056e8e19aef0f1cac58aea1a421a98f2ac732e5
Adam Halasa <adam@tworcastron.pl>
2023-04-15 12:58:22 +0200

    OOP - prototype

--- src/js/oop.js
+++ src/js/oop.js
@@ -7,15 +7,17 @@ const product = {
 // OOP:
 // kształt/struktura/klasa
 function Product(title, price) {
-  this.title = title;
+  this.name = title;
   this.price = price;
-
-  
 }
+// obiekty dziedziczą wartości z prototypu
+Product.prototype.getTitle = function() { return this.name; }
+Product.prototype.setTitle = function(title) { this.name = title; }
 
 // obiekt
 const p1 = new Product('JS od podstaw', 100);
 const p2 = new Product('PHP od podstaw', 80);
 
-console.log(p1);
+p1.setTitle('JS dla zaawansowanych');
+console.log(p1.getTitle());
 console.log(p2);
